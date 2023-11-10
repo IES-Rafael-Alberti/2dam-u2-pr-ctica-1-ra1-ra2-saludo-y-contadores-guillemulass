642e01edbb51074cc950087213ca2d0bbcef715f
guillemulass <gmulbat1301@g.educaand.es>
2023-11-10 16:44:05 +0100

    first commit

--- app/src/main/java/com/gmulbat1301/saludoycontador/MainActivity.kt
+++ app/src/main/java/com/gmulbat1301/saludoycontador/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.gmulbat1301.saludoycontador.screen.mainScreen
 import com.gmulbat1301.saludoycontador.ui.theme.SaludoYContadorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,17 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    mainScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
